6740370f708b0a63634e5a8bf3820a7c3031dd7c
Gerson Klauck <gk120hf@gmail.com>
2023-09-06 11:12:19 -0300

    Desafio Bar javaOaO

---
+++ DesfioBarJavaOaO/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+import java.utl.Locale;
+import java.util.Scanner;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		java.util.Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	}
+
+}
---
+++ DesfioBarJavaOaO/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gerson
+ *
+ */
+module DesfioBarJavaOaO {
+}
