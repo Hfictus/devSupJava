6f34a2211afd77590217c18d367919c56db877eb
Gerson Klauck <gk120hf@gmail.com>
2023-09-08 11:48:04 -0300

    Desafio Bar Java

--- DesfioBarJavaOaO/src/entities/Bill.java
+++ DesfioBarJavaOaO/src/entities/Bill.java
@@ -6,8 +6,39 @@ public class Bill {
 	public int barbecue;
 	public int softDrink;
 	
+	public double ticket() {
+		if (gender == 'F') {
+			return 8.00;
+		}
+		else {
+			return 10.00;
+			
+		}
+	}
 	
+	public double feeding() {
+		return softDrink * 3.00 + beer * 5.00 + barbecue * 7.00;
+	}
 	
+	public double covert() {
+		if(feeding() > 30.00) {
+			return 0.00;
+		}
+		else {
+			return 4.00;
+		}
+	}
 	
+	public double total() {
+		return feeding() + covert() + ticket();
+	}
+	
+	System.out.printf("\n"
+			+ "RELATÓRIO:\n"
+			+ "Couvert: R$ %.2f\n"
+			+ "Ingresso: R$ f.2f\n\n"
+			+ "Valor a pagar: R$ f.2f\n"
+			
+		);
 	
 }
