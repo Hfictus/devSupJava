808e8c2ac36ddbacf9efbf3c097f0a9ea8a562ee
Gerson Klauck <gk120hf@gmail.com>
2024-01-18 17:42:49 -0300

    DAnaliseVendas2JOO Gabarito

--- DAnaliseVendas2JOO/src/application/Program.java
+++ DAnaliseVendas2JOO/src/application/Program.java
@@ -67,7 +67,7 @@ public class Program {
 						Collectors.summingDouble(Sale::getTotal)
 					)
 				);
-	}hr
+	}
 	
 		
 	
--- DAnaliseVendas2JOO/src/application/ProgramGabarito.java
+++ DAnaliseVendas2JOO/src/application/ProgramGabarito.java
@@ -1,5 +1,69 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
+import entities.SaleGabarito;
+
 public class ProgramGabarito {
 
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Entre o caminho do arquivo: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			List<SaleGabarito> list = new ArrayList<>();
+
+			String line = br.readLine();
+			while (line != null) {
+
+				String[] fields = line.split(",");
+				int month = Integer.parseInt(fields[0]);
+				int year = Integer.parseInt(fields[1]);
+				int items = Integer.parseInt(fields[3]);
+				double total = Double.parseDouble(fields[4]);
+
+				list.add(new SaleGabarito(month, year, fields[2], items, total));
+
+				line = br.readLine();
+			}
+			
+			Map<String, Double> map = new HashMap<>();
+			for (SaleGabarito sale : list) {
+				map.put(sale.getSeller(), 0.0);
+			}
+			
+			for (String seller : map.keySet()) {
+				double total = list.stream()
+						.filter(s -> s.getSeller().equals(seller))
+						.map(s -> s.getTotal())
+						.reduce(0.0, (x, y) -> x + y);
+				map.put(seller, total);
+			}
+			
+			System.out.println();
+			System.out.println("Total de vendas por vendedor:");
+			for (String seller : map.keySet()) {
+				System.out.printf(seller + " - R$ %.2f%n", map.get(seller));
+			}
+			
+		} 
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+
+	}
 }
